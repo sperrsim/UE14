e22ac669df0dc5912b4437e152972eea5f30ceac
Simon Sperr <simon.sperr@htl-wels.at>
2021-02-18 10:30:20 +0100

    Initialised Classes

--- src/model/Person.java
+++ src/model/Person.java
@@ -5,4 +5,31 @@ package model;
  * @version 2020.3, 18.02.2021
  **/
 public class Person {
+    private String name;
+    private String address;
+    private String phonenumber;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
 }
--- src/model/Phonebook.java
+++ src/model/Phonebook.java
@@ -1,8 +1,21 @@
 package model;
 
+import java.util.List;
+
 /**
  * @author Simon Sperr
  * @version 2020.3, 18.02.2021
  **/
 public class Phonebook {
+    private List<Person> phonebook;
+
+    public void add(Person person)
+    {
+
+    }
+
+    public void delete(Person person)
+    {
+
+    }
 }
